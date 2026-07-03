91d4ca3df568621e93b7d1cb4e8b71b8ed842056
Nabil Khan <149202867+nabnab00@users.noreply.github.com>
2026-07-03 14:07:45 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES_BASE ?? '/',
+  base: '/nabnab00.github.io/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
